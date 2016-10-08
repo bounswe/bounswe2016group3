452b14cccf0292c3e8b81346b5e0d215ed9b568d
Behiye Avci <behiye.av@gmail.com>
2016-10-08 03:23:07 +0300

    android file is renewed with login page

---
+++ android/app/src/main/java/com/example/behiye/android/activities/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.example.behiye.android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Behiye on 10/8/2016.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract int getLayoutId();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutId());
+
+        ButterKnife.bind(this);
+
+    }
+
+}
