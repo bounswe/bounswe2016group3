9c1ff1ae8c08788f6de0e3fff27e28b4ea0d8a46
Arda Cinar <arda@cinar.be>
2016-10-25 21:45:19 +0300

    Javadoc for menu, meal&comment resources. Ping #121

--- backend/src/main/java/bounswegroup3/resource/MenuResource.java
+++ backend/src/main/java/bounswegroup3/resource/MenuResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import bounswegroup3.db.MealDAO;
 import bounswegroup3.db.MenuDAO;
@@ -28,23 +29,51 @@ public class MenuResource {
 		this.mealDao = mealDao;
 	}
 	
+	/**
+	 * <code>GET /api/menu/:id</code>
+	 * <br>
+	 * Gets the menu with the given id
+	 * @param id
+	 * @return A Menu object
+	 */
 	@GET
 	@Path("/{id}")
 	public Menu menuById(@PathParam("id") Long id){
 		return menuDao.getMenuById(id);
 	}
 	
+	/**
+	 * <code>GET /api/menu/:id</code>
+	 * <br>
+	 * Fetches the meals of the menu with the given id
+	 * @param id
+	 * @return A list of Meal objects
+	 */
 	@GET
 	@Path("/{id}/meals")
 	public List<Meal> mealsByMenu(@PathParam("id") Long id){
 		return mealDao.mealsByMenuId(id);
 	}
 	
+	/**
+	 * <code>POST /api/menu</code>
+	 * <br>
+	 * Creates a new menu. If you're not allowed to create that menu,
+	 * a 304 response is returned. Otherwise, a 200 response is returned
+	 * @param token Requires authentication
+	 * @param menu The Menu object - including the user id
+	 * @return The created Menu object, if successful
+	 */
 	@POST
-	public Menu createMenu(@Auth AccessToken token, @Valid Menu menu){
-		Long id = menuDao.createMenu(menu);
-		menu.setId(id);
+	public Response createMenu(@Auth AccessToken token, @Valid Menu menu){
+		if(token.getUserId()==menu.getUserId()){
+			Long id = menuDao.createMenu(menu);
+			menu.setId(id);
+			
+			return Response.ok(menu).build();
+		} else {
+			return Response.notModified().build();
+		}
 		
-		return menu;
 	}
 }
