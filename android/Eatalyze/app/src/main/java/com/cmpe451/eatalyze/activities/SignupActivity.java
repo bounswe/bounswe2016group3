e04fdae1c0016f8938c15bd287c061cd0b4a5840
Behiye Avci <behiye.av@gmail.com>
2016-10-26 13:21:20 +0300

    tabs are created for user and food server in signup page

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/SignupActivity.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/SignupActivity.java
@@ -1,14 +1,59 @@
 package com.cmpe451.eatalyze.activities;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.cmpe451.eatalyze.R;
+import com.cmpe451.eatalyze.adapters.PagerAdapter;
+
+import butterknife.Bind;
 
 /**
  * Created by Behiye on 10/17/2016.
  */
 
 public class SignupActivity extends BaseActivity {
+    @Bind(R.id.tab_layout)
+    android.support.design.widget.TabLayout tabLayout;
+    @Bind(R.id.pager)
+    ViewPager pager;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_signup;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        pager.setAdapter(adapter);
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
