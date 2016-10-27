0240deb345fc4bc0fe725f02f8c2e8a96ba325cb
Arda Cinar <arda@cinar.be>
2016-10-27 19:44:58 +0300

    Set the favicon for the frontend. Close #124

--- frontend/src/service/apiService.js
+++ frontend/src/service/apiService.js
@@ -104,7 +104,7 @@ var apiService = function(store) {
             break;
 
             case 'FOLLOW_USER':
-            apiCall("/user/"+res.id+"/follow", "POST", {"Authorization": "Bearer " + action.token}).success(function(){
+            apiCall("/user/"+action.id+"/follow", "POST", {"Authorization": "Bearer " + action.token}).success(function(){
                 apiCall("/user/"+action.id+"/following", "GET").success(function(res){
                     next({type: 'FOLLOWING_LOADED', data: res});
                 });
