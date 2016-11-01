84d4f9cfd1b8ce80a21421d60dc02cd33ae0def4
Arda Cinar <arda@cinar.be>
2016-11-01 16:25:58 +0300

    Integration test skeleton. Ping #104

---
+++ backend/src/test/java/bounswegroup3/IntegrationTest.java
@@ -0,0 +1,23 @@
+package bounswegroup3;
+
+import javax.ws.rs.client.Client;
+
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+
+public class IntegrationTest {
+	@ClassRule
+	public static final DropwizardAppRule<AppConfig> rule =
+		new DropwizardAppRule<AppConfig>(App.class, ResourceHelpers.resourceFilePath("conf.yml"));
+	
+	public static final Client client = JerseyClientBuilder.createClient();
+			
+	@Test
+	public void dummyTest() {
+		// TODO actually write that
+	}
+}
