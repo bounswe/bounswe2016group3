e9e54c63b0a1431ea3dd6b8c72d085dd200ec497
Ekrem Öztürk <ekrem.ozturk@boun.edu.tr>
2016-11-05 21:08:18 +0200

    homepage is ok for now

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/UserHomepageActivity.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/UserHomepageActivity.java
@@ -1,9 +1,7 @@
 package com.cmpe451.eatalyze.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -26,35 +25,40 @@ import retrofit.client.Response;
 
 public class UserHomepageActivity extends BaseActivity {
 
-    @Bind(R.id.hello_name)
-    TextView hello_name;
-
-    @Bind(R.id.rec_meal_list)
-    ListView rec_meal;
-
-    @Bind(R.id.rec_server_list)
-    ListView rec_server;
-
-    final List<Meal>  recMeals = new ArrayList<Meal>();
+    final List<Meal> recMeals = new ArrayList<Meal>();
 
     final List<FoodServer> recFoodServers = new ArrayList<FoodServer>();
+    @Bind(R.id.appBar)
+    Toolbar appBar;
+    @Bind(R.id.hello_name)
+    TextView helloName;
+    @Bind(R.id.tv_recommendedmeal)
+    TextView tvRecommendedmeal;
+    @Bind(R.id.rec_meal_list)
+    ListView recMealList;
+    @Bind(R.id.tv_recommendedserver)
+    TextView tvRecommendedserver;
+    @Bind(R.id.rec_server_list)
+    ListView recServerList;
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_user_homepage;
     }
 
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_user_homepage);
+        ButterKnife.bind(this);
 
         apiService.getCurrentUser(eatalyzeApplication.getAccessToken(), new Callback<User>() {
             @Override
             public void success(User user, Response response) {
                 String welcomeText = "Hello, " + user.getFullName();
-                hello_name.setText(welcomeText);
+                helloName.setText(welcomeText);
+
             }
 
             @Override
