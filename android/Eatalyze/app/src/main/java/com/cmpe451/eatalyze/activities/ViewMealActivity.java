22f7181fbfc2d060b6f13218e922ad3ddea281d8
Ekrem Öztürk <ekrem.ozturk@boun.edu.tr>
2016-11-05 20:59:44 +0200

    meal activity edited

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/ViewMealActivity.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/ViewMealActivity.java
@@ -1,9 +1,17 @@
 package com.cmpe451.eatalyze.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cmpe451.eatalyze.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -13,6 +21,21 @@ import com.cmpe451.eatalyze.R;
 public class ViewMealActivity extends BaseActivity {
 
 
+    @Bind(R.id.iv_meal_image)
+    ImageView ivMealImage;
+    @Bind(R.id.tv_meal_name)
+    TextView tvMealName;
+    @Bind(R.id.tv_server_name)
+    TextView tvServerName;
+    @Bind(R.id.btn_check_eat)
+    Button btnCheckEat;
+    @Bind(R.id.btn_tag_meal)
+    Button btnTagMeal;
+    @Bind(R.id.et_comment)
+    EditText etComment;
+    @Bind(R.id.btn_nutrition_info)
+    Button btnNutritionInfo;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_view_meal;
@@ -21,7 +44,20 @@ public class ViewMealActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_meal);
+        ButterKnife.bind(this);
+
     }
 
 
+    @OnClick({R.id.btn_check_eat, R.id.btn_tag_meal, R.id.btn_nutrition_info})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_check_eat:
+                break;
+            case R.id.btn_tag_meal:
+                break;
+            case R.id.btn_nutrition_info:
+                break;
+        }
+    }
 }
