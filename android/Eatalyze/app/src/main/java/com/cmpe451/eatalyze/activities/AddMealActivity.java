e491d08e5d4ef8e3c90366cbecb94dc42b3740fb
Behiye Avci <behiye.av@gmail.com>
2016-11-20 16:16:43 +0300

    Add new ingredients in AddMealActivity is implemented #195

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/AddMealActivity.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/AddMealActivity.java
@@ -2,7 +2,6 @@ package com.cmpe451.eatalyze.activities;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,11 +13,15 @@ import com.cmpe451.eatalyze.R;
 import com.cmpe451.eatalyze.adapters.IngredientAdapter;
 import com.cmpe451.eatalyze.models.Ingredient;
 import com.cmpe451.eatalyze.models.Meal;
+import com.squareup.okhttp.ResponseBody;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class AddMealActivity extends BaseActivity {
 
@@ -49,8 +52,14 @@ public class AddMealActivity extends BaseActivity {
 
     ArrayList<Ingredient> ingredientList = new ArrayList<Ingredient>();
     IngredientAdapter adapter;
-    double amount=0;
-    String name="";
+    double amount = 0;
+    String name = "";
+    @Bind(R.id.et_indgredient)
+    EditText etIndgredient;
+    @Bind(R.id.et_amount)
+    EditText etAmount;
+    @Bind(R.id.rl_add_ingredient_item)
+    RelativeLayout rlAddIngredientItem;
 
     @Override
     public int getLayoutId() {
@@ -69,22 +78,14 @@ public class AddMealActivity extends BaseActivity {
     //TODO add ingredients
     @OnClick(R.id.iv_add_icon)
     public void addButtonClicked() {
-        /*
-        Double amount=0.0;
-        String name="";
-        if(ingredientList.size()!=0){
-            View v=lvIngredients.getChildAt(ingredientList.size()-1);
-            amount=Double.parseDouble(((EditText)v.findViewById(R.id.et_amount)).getText().toString());
-            name=((EditText)v.findViewById(R.id.et_indgredient)).getText().toString();
-            ingredientList.get(ingredientList.size()-1).setName(name);
-            ingredientList.get(ingredientList.size()-1).setAmount(amount);
-        }else{
-            ingredientList.add(new Ingredient());
-        }
-        */
-        ingredientList.add(new Ingredient());
+        ingredientList.add(new Ingredient(etIndgredient.getText().toString(),Double.parseDouble(etAmount.getText().toString())));
         adapter.notifyDataSetChanged();
+        etIndgredient.setText("");
+        etAmount.setText("");
+        etIndgredient.setHint("ingredient");
+        etAmount.setHint("gr");
     }
+
 
     @OnClick(R.id.btn_add_meal)
     public void addMealClicked() {
@@ -93,9 +94,20 @@ public class AddMealActivity extends BaseActivity {
             ingredient = ingredient + ingredientList.get(i).getAmount() + " grams of " + ingredientList.get(i).getName() + " ";
         }
 
-        String mealName = etMealName.toString();
-        String desc = etMealDescription.toString();
+        String mealName = etMealName.getText().toString();
+        String desc = etMealDescription.getText().toString();
         //TODO get menu of different users
-        apiService.addMeal(new Meal(null, new Long(1), eatalyzeApplication.getUser().getId(), mealName, desc, ingredient, ""));
+        apiService.addMeal(new Meal(null, new Long(1), eatalyzeApplication.getUser().getId(), mealName, desc, ingredient, ""), new Callback<ResponseBody>() {
+            @Override
+            public void success(ResponseBody responseBody, Response response) {
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
     }
+
 }
