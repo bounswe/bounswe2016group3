fa156306eb3371e702da1b925a58f0d1fb3a2d43
Arda Cinar <arda@cinar.be>
2016-12-05 23:21:58 +0300

    Include/exclude dao tests

--- backend/src/main/java/bounswegroup3/db/ExcludeDAO.java
+++ backend/src/main/java/bounswegroup3/db/ExcludeDAO.java
@@ -8,10 +8,10 @@ import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 public abstract class ExcludeDAO {
 	@SqlUpdate("delete from excludes where user_id = :id")
-	public abstract void deleteUserExcludes(@Bind("id") Long id);
+	protected abstract void deleteUserExcludes(@Bind("id") Long id);
 	
 	@SqlUpdate("delete from includes where user_id = :id")
-	public abstract void deleteUserIncludes(@Bind("id") Long id);
+	protected abstract void deleteUserIncludes(@Bind("id") Long id);
 	
 	@SqlQuery("select name from excludes where user_id = :id")
 	public abstract List<String> getUserExcludes(@Bind("id") Long id);
@@ -19,11 +19,11 @@ public abstract class ExcludeDAO {
 	@SqlQuery("select name from includes where user_id = :id")
 	public abstract List<String> getUserIncludes(@Bind("id") Long id);
 	
-	@SqlUpdate("insert into excludes (name, user_id) values (:id, :name)")
-	public abstract void addExclude(@Bind("id") Long id, @Bind("name") String name);
+	@SqlUpdate("insert into excludes (name, user_id) values (:name, :id)")
+	protected abstract void addExclude(@Bind("id") Long id, @Bind("name") String name);
 	
-	@SqlUpdate("insert into includes (name, user_id) values (:id, :name)")
-	public abstract void addInclude(@Bind("id") Long id, @Bind("name") String name);
+	@SqlUpdate("insert into includes (name, user_id) values (:name, :id)")
+	protected abstract void addInclude(@Bind("id") Long id, @Bind("name") String name);
 	
 	public void updateIncludes(Long id, List<String> names) {
 		deleteUserIncludes(id);
--- backend/src/test/java/bounswegroup3/db/ExcludeDAOTest.java
+++ backend/src/test/java/bounswegroup3/db/ExcludeDAOTest.java
@@ -1,30 +1,75 @@
 package bounswegroup3.db;
 
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
 import com.amazonaws.util.json.Jackson;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import bounswegroup3.model.User;
 import bounswegroup3.utils.H2JDBIRule;
 
 public class ExcludeDAOTest {
 	@Rule
-	public H2JDBIRule db;
+	public H2JDBIRule db = new H2JDBIRule();
 	
 	private ExcludeDAO dao;
 
 	private ObjectMapper mapper;
 	
+	private ArrayList<String> data;
+	
 	@Before
-	public void setup() {
+	public void setup() throws Exception {
 		dao = db.getDbi().onDemand(ExcludeDAO.class);
+		
 		mapper = Jackson.getObjectMapper();
+		
+		User u = mapper.readValue(fixture("fixtures/user.json"), User.class);
+		db.getDbi().onDemand(UserDAO.class).addUser(u);
+		
+		data = new ArrayList<String>();
+		data.add("test");
 	}
 	
 	@After
 	public void tearDown() {
 		
+	}
+	
+	@Test
+	public void testIncludes() throws Exception {
+		List<String> res = dao.getUserIncludes(1l);
+		
+		assertThat(res.isEmpty());
+		
+		dao.updateIncludes(1l, data);
+		
+		res = dao.getUserIncludes(1l);
+		
+		assertThat(res.size()).isEqualTo(1);
+		assertThat(res.get(0)).isEqualTo("test");
+	}
+	
+	@Test
+	public void testExcludes() throws Exception {
+		List<String> res = dao.getUserExcludes(1l);
+		
+		assertThat(res.isEmpty());
+		
+		dao.updateExcludes(1l, data);
+		
+		res = dao.getUserExcludes(1l);
+		
+		assertThat(res.size()).isEqualTo(1);
+		assertThat(res.get(0)).isEqualTo("test");
 	}
 }
