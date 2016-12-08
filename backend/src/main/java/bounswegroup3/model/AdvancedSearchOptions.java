d6d5116bf4cc550a444cc492274b67eaf8b4bf00
Arda Cinar <arda@cinar.be>
2016-12-08 20:46:06 +0300

    Dummy methods for remaining calls

---
+++ backend/src/main/java/bounswegroup3/model/AdvancedSearchOptions.java
@@ -0,0 +1,76 @@
+package bounswegroup3.model;
+
+import java.util.List;
+
+public class AdvancedSearchOptions {
+	public String nameLike;
+	public String userLike;
+	public List<String> tagsList;
+	public Float maxDistance;
+	
+	public Long searcherId;
+	public Boolean usePrefs;
+	
+	public AdvancedSearchOptions() {
+		super();
+	}
+
+	public AdvancedSearchOptions(String nameLike, String userLike, List<String> tagsList, Float maxDistance,
+			Long searcherId, Boolean usePrefs) {
+		super();
+		this.nameLike = nameLike;
+		this.userLike = userLike;
+		this.tagsList = tagsList;
+		this.maxDistance = maxDistance;
+		this.searcherId = searcherId;
+		this.usePrefs = usePrefs;
+	}
+
+	public String getNameLike() {
+		return nameLike;
+	}
+	
+	public void setNameLike(String nameLike) {
+		this.nameLike = nameLike;
+	}
+	
+	public String getUserLike() {
+		return userLike;
+	}
+	
+	public void setUserLike(String userLike) {
+		this.userLike = userLike;
+	}
+	
+	public List<String> getTagsList() {
+		return tagsList;
+	}
+	
+	public void setTagsList(List<String> tagsList) {
+		this.tagsList = tagsList;
+	}
+	
+	public Float getMaxDistance() {
+		return maxDistance;
+	}
+	
+	public void setMaxDistance(Float maxDistance) {
+		this.maxDistance = maxDistance;
+	}
+	
+	public Long getSearcherId() {
+		return searcherId;
+	}
+	
+	public void setSearcherId(Long searcherId) {
+		this.searcherId = searcherId;
+	}
+	
+	public Boolean getUsePrefs() {
+		return usePrefs;
+	}
+	
+	public void setUsePrefs(Boolean usePrefs) {
+		this.usePrefs = usePrefs;
+	}
+}
---
+++ backend/src/main/java/bounswegroup3/resource/AdvancedResource.java
@@ -0,0 +1,33 @@
+package bounswegroup3.resource;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import bounswegroup3.model.AccessToken;
+import bounswegroup3.model.AdvancedSearchOptions;
+import bounswegroup3.model.Meal;
+import io.dropwizard.auth.Auth;
+
+@Path("/advanced")
+@Produces(MediaType.APPLICATION_JSON)
+public class AdvancedResource {
+	
+	@POST
+	@Path("/search")
+	public List<Meal> advancedSearch(@Auth AccessToken token, AdvancedSearchOptions opts) {
+		// TODO dummy method
+		return null;
+	}
+	
+	@GET
+	@Path("/{id}/recommend")
+	public List<Meal> recommendations(@PathParam("id") Long mealId) {
+		// TODO dummy method
+		return null;
+	}
+}
