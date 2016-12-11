6cb26c17ebc2bd7528ba5079c6b5a525d9f50234
Safa Andac <machinegunner.safa@gmail.com>
2016-12-11 10:03:19 +0300

    rateYo added

--- frontend/src/components/Meal.js
+++ frontend/src/components/Meal.js
@@ -1,114 +1,130 @@
 import React, { Component } from 'react';
-
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../actions/Meal';
 import './meal.css';
+import './rateYo/jquery.rateyo.css';
+import './rateYo/jquery.rateyo';
 import $ from 'jquery';
 
+class RateStar extends Component {
+  constructor() {
+    super();
+    this.state = {rating : 3.6};
+  }
+
+  componentDidMount() {
+    var stars = this.refs.rateYo;
+    $(stars).rateYo({
+      rating: this.state.rating,
+      onSet: function(rating) {
+        this.setState({rating: rating});
+      }.bind(this)
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p> Hello World ! </p>
+        <div ref="rateYo"></div>
+        <p> Current rating : {this.state.rating} </p>
+      </div>
+    );
+  }
+}
+
+
+
 class Meal extends Component {
-    componentDidMount() {     
+  componentDidMount() {
+  }
+
+
+  render() {
+
+
+    if(this.props.token!="" &&this.props.meal.ingredients===undefined){
+      this.props.actions.load(this.props.token,this.props.params.id);
+
+    }
+    let checkeat = () => {
+      if(this.props.token!==""){
+
+        this.props.actions.checkeat(this.props.token,this.props.params.id);
+      }
+    }
+    let comment_meal=document.getElementById("comment_meal");
+    let comment = () => {
+      if(this.props.token!==""){
+
+        this.props.actions.comment(this.props.token,this.props.params.id,this.props.currentUser.id,comment_meal.value);
+
+      }
     }
 
-  
-    render() {
-        
+    let rate_meal=document.getElementById("rate_meal");
 
-        if(this.props.token!="" &&this.props.meal.ingredients===undefined){
-            this.props.actions.load(this.props.token,this.props.params.id);    
+    let rate = () => {
+      if(this.props.token!==""){
+        $("#stars").find("#rating").each(function(){
+          alert($(this).is(':checked'));
+          if ($(this).is(':checked')==true){
 
-        }
-        let checkeat = () => {
-            if(this.props.token!==""){
-                
-                this.props.actions.checkeat(this.props.token,this.props.params.id); 
-            }
-        }
-        let comment_meal=document.getElementById("comment_meal");
-        let comment = () => {
-            if(this.props.token!==""){
-            	
-            	this.props.actions.comment(this.props.token,this.props.params.id,this.props.currentUser.id,comment_meal.value);
-            	
-            }
-        }
+          }
 
-        let rate_meal=document.getElementById("rate_meal");
 
-		let rate = () => {
-            if(this.props.token!==""){
-            	 $("#stars").find("#rating").each(function(){
-                    alert($(this).is(':checked'));
-                     if ($(this).is(':checked')==true){ 
-                       
-               }
-
-               
         });
-            this.props.actions.rate(this.props.token,this.props.params.id,this.props.currentUser.id,rate_meal.value);
-            	//ratingssHtml=this.props.ratings.average ;
-            	//document.location.href = document.location.href  ;
-            	//rateButton=<button type="button" className="btn btn-default disabled" onClick={""}>Rate</button>;
-            }
-        }        
+        this.props.actions.rate(this.props.token,this.props.params.id,this.props.currentUser.id,rate_meal.value);
+        //ratingssHtml=this.props.ratings.average ;
+        //document.location.href = document.location.href  ;
+        //rateButton=<button type="button" className="btn btn-default disabled" onClick={""}>Rate</button>;
+      }
+    }
 
-        let checkeatButton=<button type="button" className="btn btn-default" onClick={checkeat}>Check Eat!</button>;
-       
-      
-        let commentsHtml = this.props.comments.map(function(u){
-        
-            return <li key={u.id}> {u.content}</li>;
-        });
-       let ratingssHtml=this.props.ratings.average ;
-        let commentButton=<button type="button" className="btn btn-default" onClick={comment}>Comment</button>;
-        let rateButton=<button type="button" className="btn btn-default" onClick={rate}>Rate</button>;
-        return  <div className="col-xs-6">
-            
-            <h2>{this.props.meal.name}</h2>
-            {checkeatButton}
-            <p>{this.props.meal.description}</p>
-            
-            <h2>Ingredients</h2>
-            <p>{this.props.meal.ingredients}</p>
-           
-            <img src={this.props.meal.photoUrl} alt="Avatar for user {props.uid}" className="avatar-m" />
-            
-            <div className="row">
-            <div className="col-xs-6">
-            <h3> Comments </h3>
-            <p>{commentsHtml}</p>
-             <input type="text" className="form-control" placeholder="Share your thoughts" id="comment_meal" />
-             <p>{commentButton}</p>
-             </div>
-              <div className="col-xs-6">
-             <h3> Ratings </h3>
-             {ratingssHtml} out of 5
-             <input type="text" className="form-control" placeholder="Rate" id="rate_meal" />
-             <p>{rateButton}</p> 
+    let checkeatButton=<button type="button" className="btn btn-default" onClick={checkeat}>Check Eat!</button>;
 
-             </div>
-             <span className="starRating" id='stars'>
-                  <input id="rating5" type="button" name="rating" value="5" ></input>
-                  <label for="rating5">5</label>
-                  <input id="rating4" type="radio" name="rating" value="4"></input>
-                  <label for="rating4">4</label>
-                  <input id="rating3" type="radio" name="rating" value="3"></input>
-                  <label for="rating3">3</label>
-                  <input id="rating2" type="radio" name="rating" value="2"></input>
-                  <label for="rating2">2</label>
-                  <input id="rating1" type="radio" name="rating" value="1"></input>
-                  <label for="rating1">1</label>
-                </span>  
-                
-              </div>
-        
+
+    let commentsHtml = this.props.comments.map(function(u){
+
+      return <li key={u.id}> {u.content}</li>;
+    });
+    let ratingssHtml=this.props.ratings.average ;
+    let commentButton=<button type="button" className="btn btn-default" onClick={comment}>Comment</button>;
+    let rateButton=<button type="button" className="btn btn-default" onClick={rate}>Rate</button>;
+    return  <div className="col-xs-6">
+
+      <h2>{this.props.meal.name}</h2>
+      {checkeatButton}
+      <p>{this.props.meal.description}</p>
+
+      <h2>Ingredients</h2>
+      <p>{this.props.meal.ingredients}</p>
+
+      <img src={this.props.meal.photoUrl} alt="Avatar for user {props.uid}" className="avatar-m" />
+
+      <div className="row">
+        <div className="col-xs-6">
+          <h3> Comments </h3>
+          <p>{commentsHtml}</p>
+          <input type="text" className="form-control" placeholder="Share your thoughts" id="comment_meal" />
+          <p>{commentButton}</p>
         </div>
-    }
+        <div className="col-xs-6">
+          <h3> Ratings </h3>
+          {ratingssHtml} out of 5
+          <input type="text" className="form-control" placeholder="Rate" id="rate_meal" />
+          <p>{rateButton}</p>
+          <RateStar />
+        </div>
+      </div>
+
+    </div>
+  }
 
 }
 
 var mapStateToProps = function(state) {
-    return { 
+    return {
         token: state.token,
         profile: state.profile,
         meal: state.meal,
