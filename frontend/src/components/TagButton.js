2829228d3c17e1ca10194b6cb396a1d1cae9194c
Safa Andac <machinegunner.safa@gmail.com>
2016-12-14 14:54:35 +0300

    MealTag template work nearly completed

--- frontend/src/components/TagButton.js
+++ frontend/src/components/TagButton.js
@@ -9,31 +9,101 @@ import {
   ModalFooter
 } from 'react-modal-bootstrap';
 
-
-class MealTags extends Component {
+class SearchBar extends Component {
   constructor() {
     super();
     this.state = {
-      tags: ['banana', 'apple', 'go', 'nine', 'six', 'seven']
+      query_tags: ['go', 'over', 'here', 'to', 'try', ':P'],
+      results: []
+    };
+
+    this.tagConstruct = this.tagConstruct.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  tagConstruct(tag) {
+    //construct a new tag apiCall should be added...
+    this.setState({
+      query_tags: [...this.state.query_tags, tag]
+    });
+    this.props.addTag(tag);
+  }
+
+  search() {
+    const query = this.input.value;
+    var results = [];
+    if (query === "") {
+      this.setState({
+        results: []
+      });
+      return;
+    }
+    this.state.query_tags.forEach((tag) => {
+      if(tag.indexOf(query) === -1 ||
+        this.props.tags.includes(tag))  return;
+      results.push(tag);
+    });
+    this.setState({
+      results: results
+    });
+  }
+
+
+  render(){
+    const rows = this.state.results.map(result =>
+       <li className="list-group-item" key={result}>{result}</li>);
+    return (
+      <div>
+        <input type="text" id="TagInput"
+          ref={(input)=> this.input = input} onChange={this.search}
+          placeholder="Search Tag..." />
+        <ul className="list-group">
+          {rows}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+class MealTags extends Component {
+  // tags should be a prop getting from reducers.
+  // Actions for mealTags should be added...
+  constructor() {
+    super();
+    this.state = {
+      tags: ['banana', 'apple', 'go', 'nine', 'six', 'seven'],
     };
 
     this.deleteTag = this.deleteTag.bind(this);
+    this.addTag = this.addTag.bind(this);
+  }
+
+  addTag(tag) {
+    // should be changed to addTag apiCall
+    this.setState({
+      tags: [...this.state.tags, tag]
+    });
   }
 
   deleteTag() {
     console.log('Good!!!');
+    console.log('DeleteTag call should be added.');
   }
 
   render() {
     return (
-      <ul className="list-group">
-        {this.state.tags.map((tag) => (
-          <div key={tag}>
-            <button class="link" onClick={this.deleteTag}> &#x2717; </button>
-            <li className="list-group-item list-group-item-info">{tag}</li>
-          </div>
+      <div>
+        <SearchBar tags={this.state.tags} addTag={this.addTag}/>
+        <ul className="list-group">
+          {this.state.tags.map((tag) => (
+            <div key={tag}>
+              <button className="link" onClick={this.deleteTag}> &#x2717; </button>
+              <li className="list-group-item list-group-item-info">{tag}</li>
+            </div>
           ))}
-      </ul>
+        </ul>
+      </div>
     );
   }
 }
