678b0e0fbcf476ea62c1cb1dfaffcc42195f8ae1
Ekrem Ozturk <ekrem.ozturk@boun.edu.tr>
2016-12-18 23:33:39 +0300

    Closed #276

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/TagMealActivity.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/TagMealActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.cmpe451.eatalyze.R;
 import com.cmpe451.eatalyze.models.Meal;
@@ -61,22 +62,40 @@ public class TagMealActivity extends BaseActivity {
 
         Meal meal = (Meal) getIntent().getSerializableExtra("ClickedMeal");
 
-        Tag tag = new Tag(new Long(meal.getId()), etTagName.getText().toString());
+        String tagName = etTagName.getText().toString();
 
-        if(tag.getTag().length() > 0) {
-            //NOT WORKING
-            apiService.tagMeal(eatalyzeApplication.getAccessToken(), tag, new Callback<Tag>() {
-                @Override
-                public void success(Tag tag, Response response) {
-                    Log.d("tagging success", response.toString());
-                    etTagName.setText("");
-                }
-
-                @Override
-                public void failure(RetrofitError error) {
-                    Log.d("tagging failure", error.toString());
-                }
-            });
+        Long relationType = new Long(2);
+        Long relationId = meal.getId();
+        String displayName = "";
+        String identifier = "";
+        if(tagName.contains(":")){
+            int tagIndex = tagName.indexOf(":");
+            displayName = tagName.substring(0, tagIndex);
+            identifier = tagName.substring(tagIndex+1);
+        } else {
+            CharSequence text = "Please enter a proper tag (e.g name:identifier)";
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
         }
+
+        Tag tag = new Tag(relationType, relationId, displayName, identifier);
+
+        //TODO remove tagging by server check
+
+        apiService.tagMeal(eatalyzeApplication.getAccessToken(), tag, new Callback<Tag>() {
+            @Override
+            public void success(Tag tag, Response response) {
+                etTagName.setText("");
+                CharSequence text = "Meal tagged as" +tag.getDisplayName()+ " successfully";
+                Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("tagging failure", error.toString());
+            }
+        });
+
     }
 }
