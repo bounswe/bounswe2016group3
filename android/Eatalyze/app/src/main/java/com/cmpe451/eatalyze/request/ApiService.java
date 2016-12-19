c674d5e5d5a3a089bb4c80283f2ef8caad7a56c7
Ekrem Ozturk <ekrem.ozturk@boun.edu.tr>
2016-12-19 21:52:29 +0300

    Closed #212

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/request/ApiService.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/request/ApiService.java
@@ -118,9 +118,8 @@ public interface ApiService {
     @GET("/api/meal/search/{query}")
     public void mealSearch(@Path("query") String query, Callback<List<Meal>> mealListCallback);
 
-
     @GET("/api/meal/{id}/tags")
-    public void tagsByMeal(@Path("id") Long id, Callback<List<String>> tagListCallback);
+    public void tagsByMeal(@Path("id") Long id, Callback<ArrayList<Tag>> tags);
 
     @POST("/api/meal/tag")
     public void tagMeal(@Query("accessToken") AccessToken token, @Body Tag tag, Callback<Tag> tagCallback);
