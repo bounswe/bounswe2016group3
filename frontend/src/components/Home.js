2132b51ceabe1860dc77a422e963ab8702e236e3
Noor Ahmad <ahmad.noor@boun.edu.tr>
2016-12-21 00:11:47 +0300

    Design of new pages in progress

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import './Home.css';
 
 var Home = function(props){
     return (
-        <div className="jumbotron">
-            <h1>Eatalyze</h1>
-            <p>A place to simplify your nutritional needs</p>
+    	<div className="row">
+        <div className="container">
+        <img src="/logo.png" alt="Logo" id="this" />
+            <h3><strong>A place to simplify your nutritional needs</strong></h3>
+            <div className="col-sm-4">
 
+            </div>
+
+            <div className="col-sm-2">
             <p><a className="btn btn-primary" href="/signup">Signup as a regular user</a></p>
+            </div>
+            <div className="col-sm-2">
+
             <p><a className="btn btn-primary"href="/signup">Signup as a food server</a></p>
+            </div>
+            <div className="col-sm-4">
+            </div>
+        </div>
         </div>
     );
 }
