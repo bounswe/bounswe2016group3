fd80b7a7c46416a7e8d75cde9697a934f9cec2aa
Behiye Avci <behiye.av@gmail.com>
2016-12-22 14:25:30 +0300

    piechart for the macros is added

--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/SettingsActivity.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/activities/SettingsActivity.java
@@ -6,21 +6,32 @@ import android.support.v7.widget.Toolbar;
 import android.widget.LinearLayout;
 
 import com.cmpe451.eatalyze.R;
+import com.numetriclabz.numandroidcharts.ChartData;
+import com.numetriclabz.numandroidcharts.PieChartL;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @Bind(R.id.appBar)
-    Toolbar appBar;
-    @Bind(R.id.activity_settings)
-    LinearLayout activitySettings;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        ButterKnife.bind(this);
+
+        PieChartL pie = (PieChartL) findViewById(R.id.pie_chart);
+
+        List<ChartData> values = new ArrayList<>();
+
+        values.add(new ChartData("Cricket", 50f));
+        values.add(new ChartData("Football", 60f));
+        values.add(new ChartData("Hockey", 30f));
+        values.add(new ChartData("Tenis", 20f));
+        values.add(new ChartData("Rugby",40f));
+        values.add(new ChartData("Polo",10f));
+        pie.setData(values);
     }
 }
--- android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/fragments/MacroFragment.java
+++ android/Eatalyze/app/src/main/java/com/cmpe451/eatalyze/fragments/MacroFragment.java
@@ -5,16 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.cmpe451.eatalyze.R;
 import com.cmpe451.eatalyze.activities.LogActivity;
 import com.cmpe451.eatalyze.models.NutritionalInfo;
-import com.cmpe451.eatalyze.views.NutritionInfoView;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.data.PieDataSet;
+import com.numetriclabz.numandroidcharts.ChartData;
+import com.numetriclabz.numandroidcharts.PieChartL;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,52 +28,29 @@ import retrofit.client.Response;
 public class MacroFragment extends Fragment {
 
     NutritionalInfo nutritionalInfo = new NutritionalInfo();
+    @Bind(R.id.pie_chart)
+    PieChartL pieChart;
 
-    @Bind(R.id.ll_content)
-    LinearLayout llContent;
-    @Bind(R.id.pc_macro_chart)
-    PieChart pcMacroChart;
+//    @Bind(R.id.ll_content)
+//    LinearLayout llContent;
+//    @Bind(R.id.pie_chart)
+//    PieChartL pieChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_macro, container, false);
         ButterKnife.bind(this, view);
 
-        ((LogActivity) getActivity()).getApiService().getWeeklyNutritionalInfo(new Callback<NutritionalInfo>() {
-            @Override
-            public void success(NutritionalInfo nutritionalInfo, Response response) {
-                //TODO add piechart for macros
+        List<ChartData> values = new ArrayList<>();
 
-                NutritionInfoView view1 = new NutritionInfoView(getContext(), "Protein", "" + nutritionalInfo.getProtein() + "" + " gr");
-                llContent.addView(view1);
-                NutritionInfoView view2 = new NutritionInfoView(getContext(), "Total Carbohydrate", "" + nutritionalInfo.getTotalCarbohydrate() + "" + " gr");
-                llContent.addView(view2);
-                NutritionInfoView view3 = new NutritionInfoView(getContext(), "Total Fat", "" + nutritionalInfo.getTotalFat() + "" + " gr");
-                llContent.addView(view3);
+        float fat=((LogActivity)getActivity()).nutritionalInfo.getTotalFat();
+        float protein=((LogActivity)getActivity()).nutritionalInfo.getProtein();
+        float carbo=((LogActivity)getActivity()).nutritionalInfo.getTotalCarbohydrate();
+        values.add(new ChartData("Protein", protein));
+        values.add(new ChartData("Fat", fat));
+        values.add(new ChartData("Carbohydrate ", carbo));
+        pieChart.setData(values);
 
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-
-            }
-        });
-
-        /*
-        ArrayList<PieEntry> entries=new ArrayList<>();
-        entries.add(new PieEntry(4f, 0));
-        entries.add(new PieEntry(8f, 0));
-        entries.add(new PieEntry(6f, 0));
-
-        PieDataSet dataSet=new PieDataSet(entries, "Macros");
-
-        ArrayList<String> labels=new ArrayList<>();
-        labels.add("carbo");
-        labels.add("protein");
-        labels.add("fat");
-
-        PieData data=new PieData(dataSet);
-        pcMacroChart.setData(data);*/
 
         //TODO check if this usage is valid
         return view;
